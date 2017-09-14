a638dd56ebdb9e575a727826fcba0897bcb0aed6
joebui <dienbuijoe@outlook.com>
2017-09-14 08:50:34 +0700

    add bull and job and  arena for web view

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-const cron = require('node-cron')
+const Queue = require('bull')
+const config = require('config')
+const express = require('express')
+const Arena = require('bull-arena')
 
-const redis = require('./DAL/redis-connection')
 require('./DAL/mongodb-connection')
-
+const redis = require('./DAL/redis-connection')
 const stationJob = require('./jobs/station-ranking')
 const constants = require('./utilities/constants')
+const router = express.Router()
 
-stationJob.sortStations().then(value => {
-    const data = JSON.stringify(value)
-    redis.set(constants.STATION_RANKING, data)
-    console.log(data)
+const stationRankingJob = new Queue('station_ranking',
+    {
+        redis: {
+            port: config.get('redis.port'),
+            host: config.get('redis.ip')
+        }
+    }
+)
+
+// Setup Arena for webview.
+const arena = Arena(
+    {
+        "queues": [{
+            "name": "station_ranking",
+            "port": config.get('redis.port'),
+            "host": config.get('redis.ip'),
+            "hostId": "jobs"
+        }]
+    }
+)
+router.use('/', arena)
+
+stationRankingJob.process((job, done) => {
+    runStationRanking(job, done)
 })
 
-cron.schedule('0 */1 * * *', () => {
+stationRankingJob.add(null, { repeat: { cron: '1 * * * *' } })
+
+function runStationRanking(job, done) {
     stationJob.sortStations().then(value => {
         const data = JSON.stringify(value)
         redis.set(constants.STATION_RANKING, data)
         console.log(data)
+        done()
     })
-})
+}
